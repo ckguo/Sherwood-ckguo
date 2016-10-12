7b0858c5b2b878080f565fc961e08b0946210145
Courtney <ckguo@mit.edu>
2016-10-12 14:24:14 -0400

    initial commit

---
+++ cpp/demo/source/RegressionGaussian.cpp
@@ -0,0 +1,10 @@
+#include "RegressionGaussian.h"
+
+namespace MicrosoftResearch { namespace  Cambridge { namespace Sherwood
+{
+  const PixelBgr RegressionGaussianExample::DensityColor = PixelBgr::FromArgb(194, 32, 14);
+  const PixelBgr RegressionGaussianExample::DataPointColor = PixelBgr::FromArgb(224, 224, 224);
+  const PixelBgr RegressionGaussianExample::DataPointBorderColor = PixelBgr::FromArgb(0, 0, 0);
+  const PixelBgr RegressionGaussianExample::MeanColor = PixelBgr::FromArgb(0, 255, 0);
+} } }
+

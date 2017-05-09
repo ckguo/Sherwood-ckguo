5500b6b2877b3c0f0de331a2bd3147fd57fe0c6e
Courtney K Guo <courtney@hops.csail.mit.edu>
2017-05-09 03:42:00 -0400

    modified bash script

--- cpp/train.sh
+++ cpp/train.sh
@@ -4,6 +4,7 @@
 # $2 = coarseness
 # $3 = number of trees
 # $4 = depth
+# $5 = num split functions
 export SGE_LOG_PATH=/data/vision/polina/projects/cardiac/courtney/SGELogs/
 
 QR_PATH=/data/vision/polina/shared_software/sge/qsub-run
@@ -12,4 +13,4 @@ TRAIN_PATH="${DATA_PATH}train_without${1}_${2}.txt"
 TEST_PATH="${DATA_PATH}pat${1}_${2}.txt"
 DIR=data/vision/polina/projects/cardiac/courtney/Sherwood-ckguo/cpp
 
-$QR_PATH bin/linux/sw regression $TRAIN_PATH $TEST_PATH $DIR "pat${1}_${2}" /P $1 /T $3 /D $4 /verbose
+$QR_PATH bin/linux/sw regression $TRAIN_PATH $TEST_PATH $DIR "pat${1}_${2}" /P $1 /T $3 /D $4 /F $5 /verbose
